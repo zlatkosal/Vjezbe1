25bcb7e80a476ae213794d8202639be65a255451
cocadinamo <zlatkosalitrezic@gmail.com>
2023-02-24 23:01:00 +0100

    zlatko sal je sad ime

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import kotlin.math.roundToInt
 
 fun main(args: Array<String>) {
     //Zadatak 1: Ispiši svoje ime na konzolu
-    val myname = "Zlatkooooooooo"
+    val myname = "Zlatkosal"
     println("My name is $myname")
 
     //Zadatak 2: ime da bude u navodnicima
